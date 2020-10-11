9c7bc64476eb0fe9e47b84a6c7a1fbd5ff04799a
Hongjiang Qian <40163405+hjqian96@users.noreply.github.com>
2020-10-11 16:48:28 -0400

    adjust initial value scale

--- df-CA3D-m4.py
+++ df-CA3D-m4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ### Constant Acceleration Model in 3D space(9-dim)
+#Constant Acceleration Model in 3D space(9-dim)
 
 #--------------------------------------
 # library loading
@@ -12,8 +12,9 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
-#%matplotlib inline
-#%config InlineBackend.figure_format='svg'
+
+%matplotlib inline
+%config InlineBackend.figure_format='svg'
 
 #set suppress to not use scientific counting
 np.set_printoptions(suppress=True) 
@@ -22,14 +23,14 @@ np.set_printoptions(suppress=True)
 #----------------------------------------
 # Initialization Math Model
 #----------------------------------------
-cpu_start=time.perf_counter()
+start=time.perf_counter()
 
 # q1: variance of the process noise modeling the acceleration
-T=0.1; q2=1
+T=1; q2=1
 
 # qa1, qa2: variance for azimuth from sensor 1,2 resp.
 # qr, qe: variance for range and elevation resp.
-qr=2; qa1=0.7; qa2=0.2;qe=0.2
+qr=0.02; qa1=0.007; qa2=0.002;qe=0.002
 
 dimX=9; dimY=4
 N=1000; n0=50; N_sample= 1000; sigma0_train=1
@@ -46,12 +47,12 @@ F0=np.array([[1,T,T*T/2,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0,1]])
 
 x0=np.array([[21.68],
-             [-0.3],
+             [-0.0083],
              [0],
-             [10.8],
-             [-3.9],
+             [10.84],
+             [-0.39],
              [0],
-             [0.4],
+             [0.04],
              [0],
              [0]])
 
@@ -124,7 +125,6 @@ def H(x):
     
     return res
 
-
 #-------------------------------------
 # Extended KF Monte Carlo
 #-------------------------------------
@@ -161,7 +161,6 @@ def extended_kf(f,g,h,F,G,H,Q0,R0,x0,y_raw,N):
         x_bar[k+1]=x_hat[k+1]+R[k+1]@H(x_hat[k+1]).T@inv_pos@(y_raw[k+1]-h(x_hat[k+1]))
         
     return x_hat,x_bar
-
 
 #------------------------------------------------------------------
 # Generating tons of samples
@@ -220,7 +219,7 @@ def deep_filtering(datas,labels,x_hats,x_bars,x_raws,y_raws):
     
     # Data preprocessing procedure
     datas=datas.reshape(((N-n0+2)*N_sample,dimY*n0))
-    # convert numpy array into pandas dataframe
+    # convert Numpy array into pandas DataFrame
     datas=pd.DataFrame(datas)
     labels=pd.DataFrame(labels)
     x_hats=pd.DataFrame(x_hats)
@@ -268,7 +267,7 @@ def deep_filtering(datas,labels,x_hats,x_bars,x_raws,y_raws):
         return model
 
     model=build_model()
-    mymodel=model.fit(training_data,training_label, epochs=10, batch_size=16)
+    mymodel=model.fit(training_data,training_label, epochs=10, batch_size=16,verbose=0)
 
     #-------------------------------
     # Evaluation Performance
@@ -281,12 +280,11 @@ def deep_filtering(datas,labels,x_hats,x_bars,x_raws,y_raws):
     index=test_label.index.tolist()
     kf_mse_err=mean_squared_error(x_bars.iloc[index],labels.iloc[index])
 
-    cpu_end=time.perf_counter()
+    end=time.perf_counter()
 
-    #print("The mse of deep filtering is {:.3%}".format(test_mse_score))
-    #print("The mse of Kalman Filtering is {:.3%}".format(kf_mse_err))
-    #print("The CPU consuming time is {:.5}".format(cpu_end-cpu_start))
-
+    print("The test mse err of DF is {:.2%}"%(test_mse_score))
+    print("The test mse err of KF is {:.2%}"%(kf_mse_err))
+    print("The GPU consuming time is {:.3}"%(start-end))
 
     #history_dict=mymodel.history
     #history_dict.keys()
@@ -313,6 +311,7 @@ def graph_plot(N):
     data_new=np.zeros((N-n0+2,n0,dimY))
     for k in range(N-n0+2):
         data_new[k]=y_new[k:k+n0]
+
     # convert data to be consistent with deep learning.
     data_new=data_new.reshape(N-n0+2,n0*dimY)
     data_new=pd.DataFrame(data_new)
@@ -320,9 +319,9 @@ def graph_plot(N):
     # Before predict, normalize data with training information.
     data_new=(data_new-data_mean)/data_std
     df_pred=model.predict(data_new)
-    for i in range(N-n0+2):
+    for k in range(N-n0+2):
         # convect df results back to original scale.
-        df_pred[i,:]=df_pred[i,:]*label_std+label_mean
+        df_pred[k,:]=df_pred[k,:]*label_std+label_mean
 
     df_new=[x0 for k in range(n0-1)]
     df_new=np.array(df_new)
@@ -330,30 +329,34 @@ def graph_plot(N):
     df_new=np.vstack((df_new,df_pred))
     
     #--------------------------------
-
     plt.figure(dpi = 600,figsize=[40,30])
+
     axis=np.linspace(0,N+1,N+1)
     fig,ax=plt.subplots(3,3,sharex=True)
     plt.xlim((0,1))
-    ax[0][0].plot(axis, x_new[:,0]/1000,'c',axis,x_bar_new[:,0]/1000,'b',axis,df_new[:,0]/1000,'r',linewidth=0.5)
+    ax[0][0].plot(axis, x_new[:,0],'c',axis,x_bar_new[:,0],'b',axis,df_new[:,0],'r',linewidth=0.5)
     ax[0][0].set_xlim((0,N+1))
     ax[0][0].minorticks_on()
     ax[0][1].plot(axis, x_new[:,1],'c',axis,x_bar_new[:,1],'b',axis,df_new[:,1],'r',linewidth=0.5)
     ax[0][1].minorticks_on()
     ax[0][2].plot(axis, x_new[:,2],'c',axis,x_bar_new[:,2],'b',axis,df_new[:,2],'r',linewidth=0.5)
     #y 
-    ax[1][0].plot(axis, x_new[:,3]/1000,'c',axis,x_bar_new[:,3]/1000,'b',axis,df_new[:,3]/1000,'r',linewidth=0.5)
+    ax[1][0].plot(axis, x_new[:,3],'c',axis,x_bar_new[:,3],'b',axis,df_new[:,3],'r',linewidth=0.5)
     ax[1][1].plot(axis, x_new[:,4],'c',axis,x_bar_new[:,4],'b',axis,df_new[:,4],'r',linewidth=0.5)
     ax[1][2].plot(axis, x_new[:,5],'c',axis,x_bar_new[:,5],'b',axis,df_new[:,5],'r',linewidth=0.5)
     # z
-    ax[2][0].plot(axis, x_new[:,6]/1000,'c',axis,x_bar_new[:,6]/1000,'b',axis,df_new[:,6]/1000,'r',linewidth=0.5)
+    ax[2][0].plot(axis, x_new[:,6],'c',axis,x_bar_new[:,6],'b',axis,df_new[:,6],'r',linewidth=0.5)
     ax[2][1].plot(axis, x_new[:,7],'c',axis,x_bar_new[:,7],'b',axis,df_new[:,7],'r',linewidth=0.5)
     ax[2][2].plot(axis, x_new[:,8],'c',axis,x_bar_new[:,8],'b',axis,df_new[:,8],'r',linewidth=0.5)
 
-
     fig.subplots_adjust(wspace=0.6, hspace=0.3)
-    plt.savefig('6dim-plot.pdf')
-    plt.show()
+    plt.savefig('CA3D-m4.pdf')
+    #plt.show()
+
+# Run First time with sigma0_train
+datas,labels,x_hats,x_bars,x_raws,y_raws=sample_generator(Q0,R0,sigma0_train)
+model,data_mean,data_std,label_mean,label_std=deep_filtering(datas,labels,x_hats,x_bars,x_raw,y_raws)
+graph_plot(N)
 
 #-------------------------------
 # Data backup or Store
@@ -383,14 +386,16 @@ def graph_plot(N):
 #np.save(file='datas/df-CV2D-m2-x_bar_new',arr=x_bar_new)
 #np.save(file='datas/df-CV2D-m2-df_new',arr=df_new)
 
-#----------------------------
+
+#----------------------------------
 # Robustness Analysis for NM 
-#----------------------------
+#----------------------------------
 
 def robust_analysis_NM():
+
     sigma0_AM=0.5;  sigma0_NM=np.array([0.1, 0.5, 1.0, 1.5, 2.0, 2.5])
     
-    # For kalman Filter, we use NM model with recursive formula and Riccati equation.
+    # For Kalman Filter, we use NM model with recursive formula and Riccati equation.
     # But when applying KF procedure, use observation from AM that is y_n is from AM model.
     
     R0_NM=np.zeros((len(sigma0_NM),dimY,dimY))
@@ -429,18 +434,16 @@ def robust_analysis_NM():
         # Deep Filtering Error
         df_mse_err=mean_squared_error(x_raw_AM[n0-1:],df_pred)
         kf_mse_err=mean_squared_error(x_raw_AM[n0-1:],x_bar[n0-1:])
-        temp=sigma0_NM[i]
-        
-        print("For fixed sigma0_AM and sigma0_NM %.2f, the mse errs of df and kf are: %.2f,%.2f"%(temp,df_mse_err, kf_mse_err))
+        print("For fixed sigma0_AM and sigma0_NM %.2f, the mse errs of DF and KF are: %.2f,%.2f"%(sigma0_NM[i],df_mse_err, kf_mse_err))
 
-#----------------------------
+#----------------------------------------------------
 # Robustness Analysis for AM
-#----------------------------
+#----------------------------------------------------
 
 def robust_analysis_AM():
     sigma0_AM=np.array([0.1, 0.5, 1.0, 1.5, 2.0, 2.5]); sigma0_NM=0.5  
     
-    # For kalman Filter, we use NM model with recursive formula and Riccati equation.
+    # For Kalman Filter, we use NM model with recursive formula and Riccati equation.
     # But when applying KF procedure, use observation from AM that is y_n is from AM model.
     
     R0_NM=sigma0_NM*sigma0_NM*R0
@@ -480,9 +483,8 @@ def robust_analysis_AM():
         # Deep Filtering Error
         df_mse_err=mean_squared_error(x_raw_AM[n0-1:],df_pred)
         kf_mse_err=mean_squared_error(x_raw_AM[n0-1:],x_bar[n0-1:])
-        print("For fixed sigma0_NM and sigma0_AM %.2f, the mse errs of df and kf are: %.2f, %.2f"%(sigma0_AM[i],df_mse_err, kf_mse_err))  
-
-
+        print("For fixed sigma0_NM and sigma0_AM %.2f, the mse errs of DF and KF are: %.2f, %.2f"%(sigma0_AM[i],df_mse_err, kf_mse_err))  
+        
 robust_analysis_NM()
 robust_analysis_AM()
 
